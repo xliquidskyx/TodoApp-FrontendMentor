541a8565c973bbb8fc25d12333df23a44e36b90f
Aventia <lauralis0405@gmail.com>
2022-02-11 19:16:31 +0100

    fixed js and added css

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const todoInput = document.querySelector('.task-input');
 const todoSubmit = document.querySelector('.task-submit');
 const todoTask = document.querySelector('.task-element');
 
-todoSubmit = addEventListener('keypress', addTodo);
+todoSubmit.addEventListener('click', addTodo);
 
-function addTodo(e) {
-    e.preventDefault();
-    if (event.key === 'Enter') {
-        alert('hello');
-    }
+function addTodo (event) {
+    event.preventDefault(); //prevents from submitting the form
+    
+    //create a div
+    const createDiv = document.createElement('div');
+    createDiv.classList.add('todo');
+    
+    //create li
+    const createLi = document.createElement('li');
+    createLi.innerText = "it's working!!";
+    createLi.classList.add('todo-item');
+    createDiv.appendChild(createLi);
+
+    //append to list
+    todoTask.appendChild(createDiv);
 }
